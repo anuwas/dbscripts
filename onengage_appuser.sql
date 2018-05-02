9d2e0e2d581c994db8bbc868788d6da31d42ea7e
ANUWAS <info.anuwas@gmail.com>
2018-05-02 22:06:10 +0530

    commit

--- onengage_appuser.sql
+++ onengage_appuser.sql
@@ -41,23 +41,3 @@ CREATE TABLE `appuser` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `appuser`
---
-
-LOCK TABLES `appuser` WRITE;
-/*!40000 ALTER TABLE `appuser` DISABLE KEYS */;
-INSERT INTO `appuser` VALUES (1,'merchant',1,'merchant','merchant','sells','merchant','merchant@mail.com','9007711001','2017-04-30 19:18:23','',1,NULL,NULL),(2,'brand',1,'brand','brand','sells','brand','brand@mail.com','+919007711002','2017-05-11 16:25:36','YnJhbmRAbWFpbC5jb20tMi0yMDE3LTA1LTExLTA0LTA1LTM2',1,NULL,NULL),(3,'outlet',1,'outlet','outlet',NULL,'Anupam','outlet@mail.com','9007711003','2017-05-01 06:39:56',NULL,1,NULL,NULL);
-/*!40000 ALTER TABLE `appuser` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-04-28 11:00:13
